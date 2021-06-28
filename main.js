ca2c8a019d66a63d63c281dcb200d2c9b4ba60f4
AranBeitia <arantza.beitia@gmail.com>
2021-06-28 12:52:05 +0200

    scripts

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let button = document.querySelector(".button")
-button.addEventListener("click",operations)
-function operations (){
+var btn = document.querySelector(".btn");
+btn.addEventListener("click", operations);
+
+function operations (event){
+    event.preventDefault()
     console.log("Hi World")
 }
