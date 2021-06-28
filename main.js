ea96453f88726f47021f0b12509cd63325accfbe
AranBeitia <arantza.beitia@gmail.com>
2021-06-28 16:25:16 +0200

    add reset functionality

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var btns = document.querySelectorAll(".btn");
-btns.forEach(btn=>btn.addEventListener("click", operations))
+btns.forEach(btn => btn.addEventListener("click", operations))
 
 function operations (event){
-    let value = event.target.value
-    event.preventDefault()
-    
+  event.preventDefault()
+  let value = event.target.value
+  let output = document.querySelector(".output")
+  let span = document.createElement("span")
 
-    let output = document.querySelector(".output")
-    let span = document.createElement("span")
-    span.textContent = value
-    output.append(span)
-    
-    console.log(span)
+  span.textContent = value
+  output.append(span)
+  
+  if (value === 'reset') {
+    while (output.firstChild) {
+      output.removeChild(output.lastChild)
+    }
+  }
 }
