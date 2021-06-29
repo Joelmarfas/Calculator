dac35d10b8cfea4ba5182de1e5061f5b2f40359a
AranBeitia <arantza.beitia@gmail.com>
2021-06-29 10:05:26 +0200

    keys

--- main.js
+++ main.js
@@ -7,22 +7,39 @@ switchToggle.addEventListener('click', toggleTheme)
 function operations (event){
   event.preventDefault()
   let key = event.target
-  let action = key.dataset.action
-  let value = key.textContent
-  let numbers = []
-  let output = document.querySelector(".calculator__output")
+  let action = key.dataset.operation
+  // let value = key.textContent
+  // let numbers = []
+  // let output = document.querySelector(".calculator__output")
+
 
   if (!action) {
-    numbers.push(value)
-    console.log(numbers)
-    output.textContent += numbers
-    console.log(output);
-    return numbers
+    console.log('es un numero');
+    // numbers.push(value)
+    // console.log(numbers)
+    // output.textContent += numbers
+    // console.log(output);
+    // return numbers
   }
 
-  if (action === 'reset') {
-    output.textContent = 0
-  }
+  // if (action === 'reset') {
+  //   output.textContent = 0
+  // }
+
+  if(
+    action === 'add' ||
+    action === 'substract' ||
+    action === 'multiply' ||
+    action === 'divide' ||
+    action === 'module'
+    ) {
+      console.log('es un simbolo de operaciones');
+    }
+
+    if(action === 'decimal') { console.log('es decimal') }
+    if(action === 'reset' ) { console.log('resetea') }
+    if(action === 'calculate') { console.log('haz el calculo')}
+    if(action === 'int') { console.log('negativo');}
 }
 
 function toggleTheme () {
