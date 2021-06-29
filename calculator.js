efaa8ac82cf88c3d40267b41ed418bc2f8853379
AranBeitia <arantza.beitia@gmail.com>
2021-06-29 18:22:59 +0200

    second option

--- calculator.js
+++ calculator.js
@@ -6,22 +6,37 @@ switchToggle.addEventListener('click', toggleTheme)
 buttons.forEach( button => button.addEventListener('click', operations))
 
 function operations(event) {
-  event.preventDefault()
   let key = event.target
   let action = key.dataset.operation
+  let value = key.textContent
+  let outputNum = output.textContent
+  let firstValue = 0
+  let secondValue = []
 
-  if(action === 'add') addOperation()
-  if(action === 'substract') substractOperation()
-  if(action === 'multiply') multiplyOperation()
-  if(action === 'divide') divideOperation()
-  if(action === 'module') moduleOperation()
+  if(!action) {
+    if(outputNum === '0') {
+      output.textContent = value
+    } else if (outputNum.length <= 6) {
+      firstValue = outputNum + value
+      output.textContent = firstValue
+    }
+    console.log(typeof Number(firstValue))
+    return firstValue
+  }
+
+  if(action === '+') addOperation(Number(firstValue), Number(firstValue))
+  // if(action === '-') substractOperation(a, b)
+  // if(action === '*') multiplyOperation(a, b)
+  // if(action === '/') divideOperation(a, b)
+  // if(action === '%') moduleOperation(a, b)
+
 }
 
-const addOperation = () => console.log('suma')
-const substractOperation = () => console.log('resta')
-const multiplyOperation = () => console.log('multiplica')
-const divideOperation = () => console.log('divide')
-const moduleOperation = () => console.log('modulo')
+const addOperation = (v1, v2) => console.log(eval(v1 + v2))
+const substractOperation = (v1, v2) => console.log('-')
+const multiplyOperation = (v1, v2) => console.log('*')
+const divideOperation = (v1, v2) => console.log('/')
+const moduleOperation = (v1, v2) => console.log('%')
 
 function toggleTheme () {
   let main = document.querySelector('.calculator')
